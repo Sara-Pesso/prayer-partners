cbeb835a7893b6e37429b72fb66f0ab5c0ab0efd
Sara-Pesso <s.pessognelli@outlook.com>
2026-02-06 21:00:54 -0500

    add json reminder lists

--- weekly_reminders.py
+++ weekly_reminders.py
@@ -1,4 +1,5 @@
 from mass_notifications import *
+import json
 
 def check_for_weekly_reminders():
     # Email account details
@@ -14,8 +15,22 @@ def check_for_weekly_reminders():
 
     found_emails = search_email_for_prayer_requests(USERNAME, PASSWORD, IMAP_SERVER, SEARCH_STRING, NEW_MAILBOX)
     for request in found_emails:
-        MESSAGE_SUBJECT = "Prayer Request: " + date.today().strftime("%Y-%m-%d")+ " " + request['Subject']
+        MESSAGE_SUBJECT = "Reminder: " + date.today().strftime("%Y-%m-%d")+ " " + request['Subject']
         MESSAGE_CONTENT = request['Body']
-        send_mass_email(MESSAGE_SUBJECT, MESSAGE_CONTENT, USERNAME, PASSWORD, DIRECTORY)
+        # add reminder subject/content to JSON for correct day
+        # send_mass_email(MESSAGE_SUBJECT, MESSAGE_CONTENT, USERNAME, PASSWORD, DIRECTORY)
         
-check_for_weekly_reminders()
+# check_for_weekly_reminders()
+
+with open(r'E:\prayer-partners\friday_reminders.json', 'r') as file:
+    data = json.load(file)
+new_entry = {
+    "reminder_title": "example2",
+    "details":{
+        "subject":"test subject",
+        "content":"make up some content!"
+    }
+}
+data["reminders"].append(new_entry)
+with open(r'E:\prayer-partners\friday_reminders.json', 'w') as file:
+    json.dump(data, file, indent=4) 
