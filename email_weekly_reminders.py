7d66bc70cc5b9664bbcea219ee195cd5ad36fbf4
Sara-Pesso <s.pessognelli@outlook.com>
2026-02-07 13:00:02 -0500

    send weekly reminder email script; json subdir

--- email_weekly_reminders.py
+++ email_weekly_reminders.py
@@ -1,5 +1,30 @@
-
+from mass_notifications import *
+import datetime
+import json
+import os
+os.chdir(r"E:\prayer-partners")
 
 def send_weekly_reminders_each_day():
+    # Email account details
+    USERNAME = "pessognellisa20@gmail.com"
+    PASSWORD = "axyf rity yzjb hcdo" # Use an App Password
+    IMAP_SERVER = "imap.gmail.com"
+    DIRECTORY = "E:\prayer-partners\directory.xlsx"
+    today = datetime.datetime.now().strftime("%A")
+    json_file = os.path.join("weekly_reminder_json", today.lower() +"_reminders.json") 
+    print(json_file)
 
-    return
+    # E:\prayer-partners\weekly_reminder_json
+
+    ## Grab the reminders stored for whatever weekday it is
+    with open(json_file, 'r') as file:
+        reminder_data = json.load(file)
+    reminders = reminder_data["reminders"]
+    for reminder in reminders:
+        EMAIL_SUBJECT = reminder['details']['subject']
+        EMAIL_CONTENT = reminder['details']['content']
+
+        send_mass_email(EMAIL_SUBJECT, EMAIL_CONTENT, USERNAME, PASSWORD, DIRECTORY)
+    return []
+
+send_weekly_reminders_each_day()
--- test.py
+++
@@ -1,5 +0,0 @@
-import json
-import os
-os.chdir(r"E:\prayer-partners")
-with open(r'friday_reminders.json', 'r') as file:
-    data = json.load(file)
