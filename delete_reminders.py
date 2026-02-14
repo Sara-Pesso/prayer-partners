ee5bff998e0e9cc48efd59c62d226d701cf58c6f
Sara-Pesso <s.pessognelli@outlook.com>
2026-02-14 11:55:25 -0500

    funcs to delete reminders from weekday jsons

--- delete_reminders.py
+++ delete_reminders.py
@@ -2,9 +2,8 @@ import json
 import os
 os.chdir(r"E:\prayer-partners")
 
-def delete_reminder_from_json(weekday):
+def display_reminders_for_day(weekday):
     json_file = os.path.join("weekly_reminder_json", weekday.lower() +"_reminders.json") 
-    print(json_file)
 
     with open(json_file, 'r') as file:
         reminder_data = json.load(file)
@@ -12,8 +11,26 @@ def delete_reminder_from_json(weekday):
     for reminder in reminders:
         EMAIL_SUBJECT = reminder['details']['subject']
         EMAIL_CONTENT = reminder['details']['content']
-        print(EMAIL_SUBJECT)
 
     return []
 
-delete_reminder_from_json("FRIDAY")
+display_reminders_for_day("FRIDAY")
+
+def delete_reminder_from_json(reminder_title, weekday):
+    json_file = os.path.join("weekly_reminder_json", weekday.lower() +"_reminders.json") 
+
+    with open(json_file, 'r') as file:
+        reminder_data = json.load(file)
+
+    for i in range(len(reminder_data['reminders'])-1,0, -1):
+        if reminder_data['reminders'][i]['reminder_title'] == reminder_title:
+            del reminder_data['reminders'][i]
+
+    with open(json_file, 'w') as file:
+            json.dump(reminder_data, file, indent=4)
+
+    return []
+
+delete_reminder_from_json("Reminder: 2026-02-13 quick test of the weekly reminder search", "FRIDAY")
+
+# display_reminders_for_day("FRIDAY")
