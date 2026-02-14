965ff9305d8d287891d60f8a8eb21f966989f5ac
Sara-Pesso <s.pessognelli@outlook.com>
2026-02-14 12:51:53 -0500

    func to update toml entries

--- update_config_toml.py
+++ update_config_toml.py
@@ -1,1 +1,13 @@
+import toml
+import os
+os.chdir(r"E:\prayer-partners")
 
+def update_toml_entries(field, key, new_value):
+    config_info = toml.load('config.toml')
+    config_info[field][key] = new_value
+    with open('config.toml', "w") as f:
+        toml.dump(config_info, f)
+
+    return []
+
+# update_toml_entries('authorized-user', 'username', 'pessognellisa20@gmail.com')
