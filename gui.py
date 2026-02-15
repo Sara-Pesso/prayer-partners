22f1c3e8457e7b7671591bf90c8274b1b8c935cd
Sara-Pesso <s.pessognelli@outlook.com>
2026-02-15 09:32:26 -0500

    app

--- gui.py
+++ gui.py
@@ -16,21 +16,24 @@ class App:
         PASSWORD = config_info['email']['password']
         DIR = config_info['directory']['dir']
 
-        self.label1 = "Email Username:"
-        self.label = tk.Label(root, textvariable=self.label1)
-        self.label.pack(pady=5)
+        self.config_toml_entry_box("Email Username:", USERNAME)
+        self.config_toml_entry_box("Email App Passowrd:", PASSWORD)
         
+
+    def config_toml_entry_box(self, toml_entry, CONFIG_ENTRY):
+        self.label = tk.Label(root, text=toml_entry)
+        self.label.grid(row= 0, column=0, pady=5)
+
         self.entry_var = tk.StringVar()
-        self.entry_var.set(USERNAME) # Dispaly current toml entry in corresponding textbox
+        self.entry_var.set(CONFIG_ENTRY) # Dispaly current toml entry in corresponding textbox
 
         self.entry_widget = tk.Entry(root, textvariable=self.entry_var, width=30)
-        self.entry_widget.pack(pady=10)
+        self.entry_widget.grid(row=0,column=1,pady=10)
 
-        self.button = tk.Button(root, text="Reset Variable", command=self.update_toml_with_new_value)
-        self.button.pack(pady=5)
-
-        
-
+        self.button = tk.Button(root, text="Update", command=self.update_toml_with_new_value)
+        self.button.grid(row=0,column=2,pady=5)
+        return []
+     
     def update_toml_with_new_value(self):
         current_value = self.entry_var.get()
         print(f"The current value of the variable is: {current_value}")
